d1366e22dfbbe9ff4fb9fecd2efe61ea51d92418
Antonio MG <tonio.mg@gmail.com>
2013-10-06 18:27:04 +0100

    Added basic statistics pero conversation

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 import sqlite3
 
-
-def printConversationList(c):
-    """Print the list of conversations stored in DB
+def printChatList(c):
+    """Print the list of Chats stored in DB
         """
 
-    c.execute('SELECT displayname, id FROM Conversations ORDER BY id')
-    return c.fetchall();
-
-
-
-conn = sqlite3.connect("C:\Users\Antonio\Desktop\main.db")
-c = conn.cursor()
-
-while 1:
-    
-    chatsList = printConversationList(c);
+    c.execute('SELECT topic, conv_dbid FROM Chats ORDER BY id')
+    chatsList = c.fetchall();
     
     index = 0;
-    for title, idChat in chatsList:
-        print str(index) + " " + str(title)
+    for chat in chatsList:
+        print str(index) + " " + str(chat[0])
         index +=1
-
-    optionSelected = raw_input("Select Chat: ")
-    print str(chatsList[int(optionSelected)][0])
+        
+    return chatsList
+def printMessagesInChat(ChatSelectedId):
+    """ Print the list of messages from the selected Chat
+        """
     
-    #Print Messages in conversation selected
-    c.execute ('SELECT author, id, body_xml FROM Messages WHERE convo_id=?', [str(chatsList[int(optionSelected)][1])])
+    #Print Messages in Chat selected
+    c.execute ('SELECT author, id, body_xml FROM Messages WHERE convo_id=?', [str(ChatSelectedId)])
     messagesList = c.fetchall()
     print messagesList
     
@@ -35,6 +27,62 @@ while 1:
         print message
         print ""
         
-    raw_input("Press any key to continue")
+    return
+
+def printUsersInChat(ChatSelectedId):
+    
+    #Get the users of that Chat
+    c.execute ('Select identity FROM ChatMembers WHERE chatname= (SELECT identity FROM Chats WHERE id=?)', [ChatSelectedId])
+    userList = c.fetchall()
+    print userList
+    
+def printMessagesPerUserInChat(ChatSelectedId):
+    
+    c.execute('SELECT author, COUNT(*) FROM Messages WHERE convo_id=? GROUP BY author ORDER BY COUNT(*) DESC', [ChatSelectedId])
+    messagesPerUser = c.fetchall()
+    
+    sumM = sum(messagesPerUser[i][1] for i in range(len(messagesPerUser)))
+     
+    print ('Total messages: ' + str(sumM))
+    for name, messagesCount in messagesPerUser:
+        print str(name) + ': ' + str(messagesCount)
+        
+
+conn = sqlite3.connect("C:\Users\Antonio\Desktop\main.db")
+c = conn.cursor()
+
+while 1:
+    
+    #Print the list of Chats:
+    chatsList = printChatList(c)
+    
+    #Wait for the user to select a Chat
+    convSelected = raw_input("Select Chat: ")
+    convSelectedInt = int(convSelected);
+    convSelectedName = str(chatsList[convSelectedInt][0])
+    convSelectedId = chatsList[convSelectedInt][1]
+    print convSelectedName
+    
+    #Print the Chat menu
+    convOptions = ['Print all messages', 'Get number of messages per user', 'Get most popular words']
+    index = 0
+    for option in convOptions:
+        print str(index) + ") " + option
+        index +=1
+        
+    optionSelected = raw_input("Select Option: ")
+    optionSelectedInt = int(optionSelected);
+    print " "
+    
+    if optionSelectedInt == 0:
+       #printMessagesInChat(convSelectedId)
+       pass
+    elif optionSelectedInt == 1:
+        printMessagesPerUserInChat(convSelectedId)
+    elif optionSelectedInt == 2:
+        pass
+    
+    raw_input("Press any key to continue...")
+        
 
     
